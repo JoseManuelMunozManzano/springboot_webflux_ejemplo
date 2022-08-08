50cc9a1ed212e8aaa1404b93a19c7ec2365bdea2
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-08-08 19:37:31 +0200

    Manejo de contrapresión: 1. Modo Reactive Data Driver

--- src/main/java/com/jmunoz/springboot/webflux/app/controllers/ProductoController.java
+++ src/main/java/com/jmunoz/springboot/webflux/app/controllers/ProductoController.java
@@ -8,7 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 // Registrando la clase como controlador de Spring.
 // Por debajo se usa el stack reactivo en vez de usar el stack servlet que se usa en Spring Web MVC.
@@ -23,16 +26,41 @@ public class ProductoController {
 
     @GetMapping({"/listar", "/"})
     public String listar(Model model) {
-        // Se añade un map para transformar el nombre del producto en mayúsculas
         Flux<Producto> productos = dao.findAll().map(producto -> {
             producto.setNombre(producto.getNombre().toUpperCase());
             return producto;
         });
 
-        // Se va a agregar otro subscriptor, el log con el nombre
         productos.subscribe(prod -> log.info(prod.getNombre()));
 
         model.addAttribute("productos", productos);
+        model.addAttribute("titulo", "Listado de productos");
+
+        return "listar";
+    }
+
+    // Formas de manejar la contrapresión en Thymeleaf y con Flux
+    //
+    // 1. Usando una variable de contexto de Thymeleaf que permite manejar la cantidad de elementos en el buffer,
+    //    es decir, se va a configurar el tamaño del buffer en CANTIDAD DE ELEMENTOS que se van a ir emitiendo.
+    //    Esta variable de contexto de la vista se conoce como Reactive Data Driver. Es un controlador que maneja
+    //    internamente eventos con los datos del stream.
+    //    Es una de las maneras más potentes y recomendadas cuando hay algún tipo de delay o estamos recibiendo
+    //    una gran cantidad de elementos.
+    @GetMapping("/listar-datadriver")
+    public String listarDataDriver(Model model) {
+        // Se hace un delay para emular que se tarda mucho en obtener la información pedida.
+        // Como son 9 elementos y con cada uno vamos a tardar 1sg, se va a tirar 9sg para terminar.
+        Flux<Producto> productos = dao.findAll().map(producto -> {
+            producto.setNombre(producto.getNombre().toUpperCase());
+            return producto;
+        }).delayElements(Duration.ofSeconds(1));
+
+        productos.subscribe(prod -> log.info(prod.getNombre()));
+
+        // Pero instanciando la variable de contexto vamos mostrando los elementos de 2 en 2, conforme vamos
+        // teniendolos
+        model.addAttribute("productos", new ReactiveDataDriverContextVariable(productos, 2));
         model.addAttribute("titulo", "Listado de productos");
 
         return "listar";
